a9a24f933c4661735f785957913d3dcece7712e0
João Antunes <joao@zezam.io>
2023-09-04 11:43:41 +0100

    Just a base test with a supabase project with auth management starter migration, and a react native project created from the cli. Next step, integrate supabase with this react-native app

---
+++ README.md
@@ -0,0 +1,7 @@
+# Supabase configs
+
+TODO: explain here process to integrate with supabase using configs.example.ts
+
+# Database
+
+Update if needed "/supabase/migrations/\*\_user_auth_management_starter.sql"
---
+++ supabase/configs.example.ts
@@ -0,0 +1,6 @@
+// A new file called configs.ts should be created from this file with correct configs
+// For local development
+
+// For staging and production environment the file should be created by pipelines before build
+export const supabaseProjectURL = "http://localhost:54321";
+export const supbaseAnonKey = "YOUR_ANON_KEY_HERE";
