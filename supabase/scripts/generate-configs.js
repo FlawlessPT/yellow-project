9ac378f273f615d3c12683fe0eeaebc52c9a7129
João Antunes <joao@zezam.io>
2023-09-05 09:42:25 +0100

    Adding scripts to configure connection to supabase

---
+++ supabase/scripts/generate-configs.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+require("dotenv").config({ path: ".env" });
+
+const configsFilePath = process.argv[2];
+
+const supabaseProjectURL = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPBASE_API_KEY;
+
+console.log("SUPABASE_URL: ", supabaseProjectURL);
+console.log("SUPBASE_API_KEY: ", supabaseAnonKey);
+
+function createConfigsFile() {
+  const writeStream = fs.createWriteStream(configsFilePath);
+  writeStream.write(
+    `export const supabaseProjectURL = '${supabaseProjectURL}';\n`
+  );
+  writeStream.write(`export const supabaseAnonKey = '${supabaseAnonKey}';\n`);
+  writeStream.end();
+  console.log("Configs file was created.");
+}
+
+if (supabaseProjectURL && supabaseAnonKey) {
+  fs.stat(configsFilePath, function (err) {
+    if (err) {
+      createConfigsFile();
+    } else {
+      fs.unlink(configsFilePath, function (err) {
+        if (err) return console.log(err);
+        console.log(
+          `${configsFilePath} file deleted successfully. A new one will be created.`
+        );
+
+        createConfigsFile();
+      });
+    }
+  });
+} else {
+  console.err(
+    "Not all needed environment variables were defined: SUPABASE_URL, SUPBASE_API_KEY"
+  );
+}
