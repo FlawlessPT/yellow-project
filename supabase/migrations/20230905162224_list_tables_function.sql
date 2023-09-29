5fea77ace1c296ccab812a494d0ed80582b819ca
João Antunes <joao@zezam.io>
2023-09-29 16:08:35 +0100

    Dev dependencies needed for .apk build

--- supabase/migrations/20230905162224_list_tables_function.sql
+++ supabase/migrations/20230905162224_list_tables_function.sql
@@ -1,3 +1,4 @@
+-- inspiration from here: https://github.com/orgs/supabase/discussions/8948
 create or replace function get_all_table_name()
 returns TABLE (table_name text)
 as $$
--- supabase/migrations/20230906084736_get_table_types.sql
+++ supabase/migrations/20230906084736_get_table_types.sql
@@ -1,3 +1,4 @@
+-- inspiration from https://stackoverflow.com/questions/74994416/how-to-get-a-list-of-postgres-types-from-supabase-table
 CREATE OR REPLACE FUNCTION get_types(tname text)
     RETURNS TABLE ( column_name text,data_type text,is_nullable text ) AS
     $BODY$
