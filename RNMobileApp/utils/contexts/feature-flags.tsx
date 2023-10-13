bccea03eac7ed58b9f0c33713dd9081bbd565184
João Antunes <joao@zezam.io>
2023-10-13 15:18:27 +0100

    Feature flag by roles

--- RNMobileApp/utils/contexts/feature-flags.tsx
+++ RNMobileApp/utils/contexts/feature-flags.tsx
@@ -6,11 +6,18 @@ type FeatureFlagType = {
   key: string;
   active: boolean;
   users?: string[];
+  roles?: string[];
+};
+
+type SignedInUserType = {
+  id: string;
+  roles?: string[];
 };
 
 const DEFAULT_REFRESH_FEATURE_FLAGS_IN_MS = 60000;
 const FeatureFlagsContext = createContext<{
   featureFlags: FeatureFlagType[];
+  signedInUser?: SignedInUserType;
 }>({featureFlags: []});
 
 export const FeatureFlagsContextProvider = ({
@@ -20,16 +27,52 @@ export const FeatureFlagsContextProvider = ({
   refreshInMsc?: number;
 }) => {
   const [featureFlags, setFeatureFlags] = useState<FeatureFlagType[]>([]);
+  const [signedInUser, setSignedInUser] = useState<SignedInUserType>();
+
+  useEffect(() => {
+    const updateSignedInUser = async (session: Session | null) => {
+      let user: SignedInUserType | null = null;
+      if (session?.user.id) {
+        user = {
+          id: session.user.id,
+        };
+        const {data} = await supabase
+          .from('profiles')
+          .select('id, roles')
+          .match({id: session.user.id})
+          .single();
+
+        if (data) {
+          user.roles = data.roles;
+        }
+      }
+
+      setSignedInUser(user || undefined);
+    };
+
+    supabase.auth.getSession().then(({data: {session: s}}) => {
+      updateSignedInUser(s);
+    });
+
+    supabase.auth.onAuthStateChange((_event, s) => {
+      updateSignedInUser(s);
+    });
+  }, []);
 
   useEffect(() => {
     const fetchFeatureFlags = async () => {
       const {data} = await supabase
         .from('feature_flags')
-        .select('key, users_ids, active');
+        .select('key, users_ids, active, roles');
 
       if (data) {
         setFeatureFlags(
-          data.map(f => ({key: f.key, active: f.active, users: f.users_ids})),
+          data.map(f => ({
+            key: f.key,
+            active: f.active,
+            users: f.users_ids,
+            roles: f.roles,
+          })),
         );
       }
     };
@@ -40,34 +83,31 @@ export const FeatureFlagsContextProvider = ({
   }, []);
 
   return (
-    <FeatureFlagsContext.Provider value={{featureFlags}}>
+    <FeatureFlagsContext.Provider value={{featureFlags, signedInUser}}>
       {children}
     </FeatureFlagsContext.Provider>
   );
 };
 
 export const useFeatureFlag = ({featureFlagKey}: {featureFlagKey: string}) => {
-  const [session, setSession] = useState<Session | null>(null);
-  const featureFlagsData = useContext(FeatureFlagsContext);
-  const featureFlag = featureFlagsData.featureFlags.find(
-    f => f.key === featureFlagKey,
-  );
+  const {featureFlags, signedInUser} = useContext(FeatureFlagsContext);
+  const featureFlag = featureFlags.find(f => f.key === featureFlagKey);
 
-  useEffect(() => {
-    supabase.auth.getSession().then(({data: {session: s}}) => {
-      setSession(s);
-    });
+  const isForAnyUser = !featureFlag?.users?.length;
+  const isForAnyRole = !featureFlag?.roles?.length;
+  const isFeatureFlagForAnyone = isForAnyUser && isForAnyRole;
 
-    supabase.auth.onAuthStateChange((_event, s) => {
-      setSession(s);
-    });
-  }, []);
+  const isUserMatched =
+    signedInUser &&
+    (isForAnyUser || featureFlag.users?.includes(signedInUser.id));
+  const isRolesMatched =
+    signedInUser &&
+    (isForAnyRole ||
+      featureFlag.roles?.some(r => signedInUser.roles?.includes(r)));
+  const isProfiledMatched = isUserMatched && isRolesMatched;
 
   return {
-    isActive: !featureFlag?.users?.length
-      ? !!featureFlag?.active
-      : !!session?.user.id &&
-        featureFlag?.users.includes(session?.user.id) &&
-        !!featureFlag?.active,
+    isActive:
+      featureFlag?.active && (isFeatureFlagForAnyone || isProfiledMatched),
   };
 };
