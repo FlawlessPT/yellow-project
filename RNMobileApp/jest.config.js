efb7d93dc1dbe5692f3aa563fb9d72a5ab24729c
João Antunes <joao.antunes@mobiweb.pt>
2023-11-07 11:52:29 +0000

    Merged in jest-test-rnmobileapp (pull request #32)
    Jest test rnmobileapp
    
    * Fixing unit test errors
    
    * Adding test for RNMobileApp on pipelines
    
    * mock supabase
    
    * mock supabase

--- RNMobileApp/jest.config.js
+++ RNMobileApp/jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: 'react-native',
+  // https://jestjs.io/docs/tutorial-react-native#transformignorepatterns-customization
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|react-native-onesignal|native-base|@sentry/.*)',
+  ],
+  // setting up needed mocks and other stuff
+  setupFiles: ['<rootDir>/jest.setup.js'],
 };
---
+++ RNMobileApp/jest.setup.js
@@ -0,0 +1,44 @@
+import {jest} from '@jest/globals';
+import mockLocalize from 'react-native-localize/mock';
+
+// Needed to solve issue with react-navigation
+jest.useFakeTimers();
+
+// Native modules need to be mocked: https://jest-bot.github.io/jest/docs/tutorial-react-native.html#mock-native-modules-using-jestmock
+
+jest.mock('react-native-localize', () => mockLocalize);
+
+// https://stackoverflow.com/a/70724310
+jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
+jest.mock('expo-secure-store', () => ({
+  getItemAsync: jest.fn(),
+  setItemAsync: jest.fn(),
+  deleteItemAsync: jest.fn(),
+}));
+jest.mock('expo-web-browser', () => ({
+  openBrowserAsync: jest.fn(),
+  dismissBrowser: jest.fn(),
+  openAuthSessionAsync: jest.fn(),
+  dismissAuthSession: jest.fn(),
+}));
+jest.mock('react-native-onesignal', () => ({
+  LogLevel: {
+    None: 0,
+    Fatal: 1,
+    Error: 2,
+    Warn: 3,
+    Info: 4,
+    Debug: 5,
+    Verbose: 6,
+  },
+  OneSignal: {
+    initialize: jest.fn(),
+    Notifications: {
+      requestPermission: jest.fn(),
+      addEventListener: jest.fn(),
+    },
+    Debug: {
+      setLogLevel: jest.fn(),
+    },
+  },
+}));
