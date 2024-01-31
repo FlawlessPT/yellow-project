0aa97734c2a0148f705f869ae2317b0edc892065
Cristiana Campos <nb29622@MAC.home>
2024-01-31 22:55:47 +0000

    refactor button component

--- RNMobileApp/components/Button/styles.ts
+++ RNMobileApp/components/Button/styles.ts
@@ -1,39 +1,32 @@
-// Styled Components
+/* Styled Components */
 import styled from 'styled-components/native';
 
-// Types
-import { ButtonType } from '.';
+/* External Libs */
+import Lottie from 'lottie-react-native';
 
 interface DefaultButtonProps {
-  type?: ButtonType;
-  disabled?: boolean;
+  backgroundColor?: string;
+  hasBorder?: boolean;
+  borderColor?: string;
 }
 
 export const DefaultButton = styled.TouchableOpacity<DefaultButtonProps>`
-  display: flex;
-  height: 50px;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  border: ${({ hasBorder, borderColor, theme }) =>
+    hasBorder ? `3px solid ${borderColor || theme.colors.primary}` : '0px'};
   border-radius: 42px;
-  position: relative;
-  overflow: hidden;
-  border: ${({ type }) => (type === 'outlined' ? '3px' : '0px')};
-  background-color: ${({ theme, type, disabled }) =>
-    disabled
-      ? theme.colors.disabled
-      : type === 'normal'
-      ? theme.colors.primary
-      : 'transparent'};
-`;
-
-export const Container = styled.View`
-  flex: 1;
-  padding: 0px 16px;
+  height: 48px;
   justify-content: center;
   align-items: center;
+  width: 100%;
+  box-shadow: 0px 1px rgba(0, 0, 0, 0.2);
+  shadow-color: ${({ theme }) => theme.colors.black};
+  shadow-opacity: 0.2;
+  elevation: 4;
 `;
 
-export const Label = styled.Text<DefaultButtonProps>`
-  font-family: Poppins-Regular;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
+export const Loading = styled(Lottie)`
+  width: 20px;
+  height: 20px;
+  align-self: center;
 `;
