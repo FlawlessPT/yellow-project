5e84ecd5b4925119302b9071dccdee81034d0392
Cristiana Campos <nb29622@MAC.home>
2024-05-09 22:19:28 +0100

    create and fix bottom tab bar

--- RNMobileApp/src/navigation/AppStack/index.tsx
+++ RNMobileApp/src/navigation/AppStack/index.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable react/no-unstable-nested-components */
 // React
 import React from 'react';
+import { Image, ImageSourcePropType, StyleSheet, TouchableOpacity } from 'react-native';
 
 // Screens
 import Home from '@screens/Home';
@@ -12,18 +14,23 @@ import SettingsStack from './SettingsStack';
 import useTheme from '@hooks/theme/useTheme';
 
 // External Libs
-import Icon from 'react-native-vector-icons/FontAwesome6';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 // Types
 import { AppStackEnum, defaultScreenOptions, RootStackEnum } from '../types';
 
+// Assets
+import { BottomDumbbellIcon, BottomFoodIcon, BottomHomeIcon, BottomPlusIcon, BottomProfileIcon } from '@assets';
+import { Theme } from '@theme';
+
 export default function AppStack(): JSX.Element {
   const Tab = createBottomTabNavigator();
   const { theme } = useTheme();
 
-  const renderIcon = (icon: string, focused: boolean) => {
-    return <Icon name={icon} size={20} color={focused ? theme.colors.primary : theme.colors.icon} />;
+  const styles = getStyles(theme);
+
+  const renderIcon = (icon: ImageSourcePropType, focused: boolean) => {
+    return <Image source={icon} style={{ tintColor: focused ? theme.colors.primary : theme.colors.icon }} />;
   };
 
   return (
@@ -32,13 +39,7 @@ export default function AppStack(): JSX.Element {
       initialRouteName={AppStackEnum.HOME}
       screenOptions={() => ({
         tabBarShowLabel: false,
-        tabBarStyle: {
-          backgroundColor: theme.colors.background,
-          borderTopWidth: 1.5,
-          borderTopColor: theme.colors.border,
-          height: 62,
-          paddingTop: 10,
-        },
+        tabBarStyle: styles.tabNavigator,
         ...defaultScreenOptions,
       })}
     >
@@ -46,30 +47,58 @@ export default function AppStack(): JSX.Element {
         name="Home"
         component={Home}
         options={{
-          tabBarIcon: ({ focused }) => renderIcon('house', focused),
+          tabBarIcon: ({ focused }) => renderIcon(BottomHomeIcon, focused),
         }}
       />
       <Tab.Screen
         name="Workout"
         component={Settings}
         options={{
-          tabBarIcon: ({ focused }) => renderIcon('dumbbell', focused),
+          tabBarIcon: ({ focused }) => renderIcon(BottomDumbbellIcon, focused),
+        }}
+      />
+      <Tab.Screen
+        name="Add"
+        component={Home}
+        options={{
+          tabBarIcon: () => <Image source={BottomPlusIcon} />,
+          tabBarButton: ({ children, onPress }) => (
+            <TouchableOpacity style={styles.button} onPress={onPress}>
+              {children}
+            </TouchableOpacity>
+          ),
         }}
       />
       <Tab.Screen
         name="Food"
         component={Settings}
         options={{
-          tabBarIcon: ({ focused }) => renderIcon('utensils', focused),
+          tabBarIcon: ({ focused }) => renderIcon(BottomFoodIcon, focused),
         }}
       />
       <Tab.Screen
         name="SettingsStack"
         component={SettingsStack}
         options={{
-          tabBarIcon: ({ focused }) => renderIcon('gear', focused),
+          tabBarIcon: ({ focused }) => renderIcon(BottomProfileIcon, focused),
         }}
       />
     </Tab.Navigator>
   );
 }
+
+const getStyles = (theme: Theme) =>
+  StyleSheet.create({
+    button: {
+      bottom: 10,
+      marginHorizontal: 40,
+    },
+    tabNavigator: {
+      backgroundColor: theme.colors.background,
+      borderTopWidth: 1.5,
+      borderTopColor: theme.colors.border,
+      height: 72,
+      paddingTop: 10,
+      position: 'absolute',
+    },
+  });
