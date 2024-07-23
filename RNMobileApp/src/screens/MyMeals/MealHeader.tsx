07bf9aacf8d486bf901d02b813399de712318410
Cristiana Campos <nb29622@CRIS-MAC.home>
2024-07-23 21:59:59 +0100

    create MyMeals screen

---
+++ RNMobileApp/src/screens/MyMeals/MealHeader.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import { Label } from '@components';
+
+import useTheme from '@hooks/theme/useTheme';
+
+import { Theme } from '@theme';
+
+const MealHeader = () => {
+  const { theme } = useTheme();
+
+  const styles = getStyles(theme);
+
+  return (
+    <View style={styles.notesContainer}>
+      <View style={styles.container}>
+        <Label text="water" color={theme.colors.white} semibold />
+        <View style={styles.timeContainer}>
+          <Label text="3,5" color={theme.colors.neutral100} bold />
+          <Label text="l" medium type="footnote" color={theme.colors.neutral100} style={styles.minLabel} />
+        </View>
+        <Label text="per day" type="footnote" color={theme.colors.neutral300} />
+      </View>
+      <View style={[styles.container, { flex: 1 }]}>
+        <Label text="observations" color={theme.colors.white} semibold />
+        <Label
+          text="Zero drinks and sauces in moderation"
+          type="footnote"
+          color={theme.colors.neutral300}
+          style={styles.notesLabel}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default MealHeader;
+
+const getStyles = (theme: Theme) =>
+  StyleSheet.create({
+    container: {
+      backgroundColor: theme.colors.neutral800,
+      padding: 18,
+      borderRadius: 18,
+    },
+    minLabel: {
+      marginLeft: 4,
+    },
+    timeContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginTop: 5,
+      marginBottom: 2,
+    },
+    notesLabel: {
+      marginTop: 8,
+    },
+    notesContainer: {
+      marginHorizontal: 16,
+      flexDirection: 'row',
+      gap: 8,
+      marginBottom: 16,
+    },
+  });
---
+++ RNMobileApp/src/screens/MyMeals/index.tsx
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import { Image, StyleSheet, TouchableOpacity, View } from 'react-native';
+
+import { InfoCircleIcon, NoWorkoutsImage } from '@assets';
+
+import { WorkoutCalendar, Label, Page, MealCard } from '@components';
+
+import MealHeader from './MealHeader';
+import meals from './stub.json';
+import useTheme from '@hooks/theme/useTheme';
+
+import { Theme } from '@theme';
+
+const MyMeals = () => {
+  const { theme } = useTheme();
+
+  const styles = getStyles(theme);
+
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+
+  return (
+    <>
+      <Page
+        withoutHorizontalMargin
+        title="diet_nutrition.title"
+        right={
+          <TouchableOpacity onPress={() => setIsModalVisible(true)}>
+            <Image source={InfoCircleIcon} />
+          </TouchableOpacity>
+        }
+      >
+        <WorkoutCalendar />
+        {meals.length ? (
+          <>
+            <MealHeader />
+            {meals?.map((meal, index) => <MealCard key={index} item={meal} />)}
+          </>
+        ) : (
+          <View style={styles.noWorkoutsContainer}>
+            <Label
+              type="h3"
+              text="enjoy.restday_title"
+              color={theme.colors.neutral500}
+              semibold
+              textAlign="center"
+              style={styles.noWorkoutsLabel}
+            />
+            <Image source={NoWorkoutsImage} />
+          </View>
+        )}
+      </Page>
+      {/* <Modal isVisible={isModalVisible}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalHeader}>
+            <Label type="h3" text="summary.week_title" color={theme.colors.white} bold />
+            <TouchableOpacity onPress={() => setIsModalVisible(false)}>
+              <Image source={CloseIcon} />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.row}>
+            <Label text="ABS" color={theme.colors.white} bold />
+            <Label text="•" color={theme.colors.white} style={styles.separator} />
+            <Label text="6 séries" color={theme.colors.white} type="footnote" />
+          </View>
+        </View>
+      </Modal> */}
+    </>
+  );
+};
+
+export default MyMeals;
+
+const getStyles = (theme: Theme) =>
+  StyleSheet.create({
+    workoutTitle: {
+      marginBottom: 16,
+    },
+    noWorkoutsLabel: {
+      marginVertical: 16,
+    },
+    noWorkoutsContainer: {
+      alignItems: 'center',
+      marginHorizontal: 38,
+    },
+    modalContainer: {
+      backgroundColor: theme.colors.neutral800,
+      borderRadius: 12,
+      padding: 20,
+    },
+    modalHeader: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginBottom: 24,
+      alignItems: 'center',
+    },
+    separator: {
+      marginHorizontal: 16,
+    },
+    row: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+  });
