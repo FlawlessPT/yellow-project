956aecffdc7aba227748215d71283ead865a9164
João Sousa <nb28117@Mac.lan>
2025-02-04 15:44:26 +0000

    improve script

--- BackofficeCMS/scripts/generate-npmrc.js
+++ BackofficeCMS/scripts/generate-npmrc.js
@@ -1,9 +1,11 @@
 import { writeFileSync } from 'fs';
 
+const npmAuthToken = process.env.NPM_AUTH_TOKEN || process.argv[2];
+
 const npmrcContent = `
   always-auth=true
   @flawlesspt:registry=https://npm.pkg.github.com/
-  //npm.pkg.github.com:_authToken=${process.env.NPM_AUTH_TOKEN}
+  //npm.pkg.github.com:_authToken=${npmAuthToken}
   registry=https://registry.npmjs.org
 `;
 
