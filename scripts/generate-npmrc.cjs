19be333942fc6213780b1d463a35739037f6bb6d
João Sousa <nb28117@Mac.lan>
2025-02-06 12:47:56 +0000

    fix: fix yarn not working to install dependencies

--- scripts/generate-npmrc.cjs
+++ scripts/generate-npmrc.cjs
@@ -3,11 +3,10 @@ const { writeFileSync } = require("fs");
 // This token is either grabbed from the environment variables (through vercel ci/cd) or passed as an argument (when running locally)
 const npmAuthToken = process.env.NPM_AUTH_TOKEN || process.argv[2];
 
-const npmrcContent = `
-  always-auth=true
-  @flawlesspt:registry=https://npm.pkg.github.com/
-  //npm.pkg.github.com:_authToken=${npmAuthToken}
-  registry=https://registry.npmjs.org
+const npmrcContent = `always-auth=true
+@flawlesspt:registry=https://npm.pkg.github.com/
+//npm.pkg.github.com/:_authToken=${npmAuthToken}
+registry=https://registry.npmjs.org
 `;
 
 // write file to .npmrc in BackOfficeCMS
